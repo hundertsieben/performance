83b24e54b719c3297210194e7b876354bb296af8
Nora Marcus <nora_lynn.marcus@tu-dresden.de>
2017-05-29 12:06:45 +0300

    parallel added

--- pi_par.c
+++ pi_par.c
@@ -1,15 +1,24 @@
-#include <stdio.h>
+# include <stdio.h>
+# include <time.h>
 
 int main() {
     int r[2800 + 1];
     int i, k;
     int b, d;
     int c = 0;
+    int start, end;
+    int delta;
 
+
+    start=clock();
+    ///////////////////////////
+    ////////BENCHMARK//////////
+    # pragma omp parallel for
     for (i = 0; i < 2800; i++) {
         r[i] = 2000;
     }
 
+    # pragma omp for ordered schedule(dynamic)
     for (k = 2800; k > 0; k -= 14) {
         d = 0;
 
@@ -24,10 +33,19 @@ int main() {
             if (i == 0) break;
             d *= i;
         }
+
+	# pragma omp ordered
         printf("%.4d", c + d / 10000);
         c = d % 10000;
     }
-    printf("\n");
+    ////////////////////////////
+    end=clock();
+    
+
+    printf("\n\n");
+    delta = end - start;
+    printf("execution time in seconds: %d", delta);
+    printf("\n\n");
     return 0;
 }
 
