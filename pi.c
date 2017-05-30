51eb1c080fef83aefe24694823c7816f3d00a86a
Nora Marcus <nora_lynn.marcus@tu-dresden.de>
2017-05-30 14:00:44 +0200

    zeug

--- pi.c
+++ pi.c
@@ -2,34 +2,20 @@
 # include <time.h>
 # include "omp.h"
 
+double calculate_pi(int);
+
 int main() {
-    
-    int i, num_steps;
-    double sum, x, step, pi;
-    
     int j, iterations = 100;
     double time[iterations];
-    double avg, start, end, delta;
+    double avg, start, end, delta, pi;
 
-    num_steps=3;
-
-    printf("parallel pi digits calculation benchmark\nexecution time in seconds: ");
+    printf("parallel pi calculation benchmark\nexecution time in seconds: ");
     for (j=0; j < iterations; j++){
+
 	    start=omp_get_wtime();
 	    ///////////////////////////
 	    ////////BENCHMARK//////////
-	    x=0;
-            sum = 0.0;
-            step = 1.0/(double)num_steps;
-            #pragma omp parallel private(i,x) 
-            {
-            #pragma omp for reduction(+:sum) schedule(static)
-            for (i=0; i<num_steps; i=i+1){
-            x=(i+0.5)*step;
-            sum = sum + 4.0/(1.0+x*x);
-            }
-            }
-            pi=step*sum;
+	    pi=calculate_pi(2048);
 	    ///////////////////////////
 	    end=omp_get_wtime();
 
@@ -40,11 +26,28 @@ int main() {
     for (j=0; j<iterations; j++){
         avg+=time[j];
     }
-
+    
     avg=avg/iterations;
-    printf("%f\n", avg);
+    printf("%.15f, and pi is: %f\n", avg, pi);
 
     return 0;
 }
 
+double calculate_pi(int num_steps){
 
+    int i;
+    double sum, x, step;
+   
+    x=0;
+    sum = 0.0;
+    step = 1.0/(double)num_steps;
+    #pragma omp parallel private(i,x) 
+    {
+      	#pragma omp for reduction(+:sum) schedule(static)
+       	for (i=0; i<num_steps; i=i+1){
+        x=(i+0.5)*step;
+        sum = sum + 4.0/(1.0+x*x);
+       	}
+    }
+    return step*sum;
+}
