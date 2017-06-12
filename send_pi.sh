e28eb16386e428ae3109bd17aaa620902e918733
Nora Marcus <nora_lynn.marcus@tu-dresden.de>
2017-06-12 10:13:46 +0200

    dunno

--- send_pi.sh
+++ send_pi.sh
@@ -4,14 +4,23 @@
 #SBATCH --nodes=1
 #SBATCH --exclusive
 #SBATCH --time=00:02:00
+#SBATCH --cpus-per-task=12
+#SBATCH --tasks-per-node=1
 
 number_threads=12
 number_iterations=100
 
+cd /scratch/s7330426/
+cp /home/s7330426/performance/pi ./
+
 module load perf/r94
+
 export OMP_NUM_THREADS=$number_threads
 
-perf record -e 'cs'  srun ./pi $number_iterations
+#perf record -o ./perf.data -e branches,branch-misses,bus-cycles,cache-misses,cache-references,cpu-cycles,instructions,ref-cycles srun ./pi $number_iterations
 
+srun perf record -o ./perf.data ./pi $number_iterations
 
-exif
+cp ./perf.data /home/s7330426/performance/
+
+exit
